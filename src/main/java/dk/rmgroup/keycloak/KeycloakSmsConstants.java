411fe53b2f67df84e23636e70d9e4e058bf4d634
Mads Stahnke <mads.stahnke@f24.com>
2025-07-04 14:48:58 +0200

    feat: add one time sms provider

--- src/main/java/dk/rmgroup/keycloak/KeycloakSmsConstants.java
+++ src/main/java/dk/rmgroup/keycloak/KeycloakSmsConstants.java
@@ -7,6 +7,10 @@ public class KeycloakSmsConstants {
     public static final String CONF_SMS_API_KEY = "sms-reset-password.api-key";
     public static final String CONF_SMS_API_SECRET = "sms-reset-password.api-secret";
     public static final String CONF_SMS_FROM = "sms-reset-password.from";
+    public static final String CONF_SMS_PROVIDER = "sms-reset-password.provider";
+    public static final String CONF_SMS_BLUEIDEA_EMAIL = "sms-reset-password.blueidea.email";
+    public static final String CONF_SMS_BLUEIDEA_PASSWORD = "sms-reset-password.blueidea.password";
+    public static final String CONF_SMS_BLUEIDEA_PROFILE_ID = "sms-reset-password.blueidea.profile-id";
 
     // SMS field names
     public static final String CONF_SMS_FIELD_TO = "sms-reset-password.field.to";
@@ -22,4 +26,9 @@ public class KeycloakSmsConstants {
     // Messages
     public static final String MESSAGE_SMS_SEND = "smsSentMessage";
     public static final String MESSAGE_SMS_SEND_ERROR = "smsSentErrorMessage";
+    public static final String MESSAGE_SMS_CODE_MESSAGE = "smsCodeMessage";
+
+    // Providers
+    public static final String CONF_SMS_PROVIDER_VONAGE = "Vonage";
+    public static final String CONF_SMS_PROVIDER_BLUEIDEA = "Blueidea";
 }
